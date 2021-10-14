56b4ee61db60af7f8a0a9e9388d7ab3fcc930789
“leonshimizu” <lmshimizu@gmail.com>
2021-10-13 17:18:20 -0700

    started first letter method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   
   def tester
-    render json: {message: "hello"}
+    render json: {message: "test"}
+  end
+
+  def first_letter
+    render json: {message: "Hello"}
   end
 end
